2746e54c629ece65d9107624552808ef8c10c885
nyoshimatsu <nyoshimatsu@isit.or.jp>
2017-08-29 18:00:58 +0900

    ポリゴンのエリアがオーバラップしている場合、より広域のエリアを塗り分け時にスキップするよう変更

--- dashboard/app/services/geo/polygon_data_builder_service.rb
+++ dashboard/app/services/geo/polygon_data_builder_service.rb
@@ -4,6 +4,7 @@ class Geo::PolygonDataBuilderService < ServiceBase
   def initialize(geojson, rows)
     @geojson = geojson
     @rows = rows
+    @rows_s = []
   end
 
   def perform
@@ -24,13 +25,60 @@ class Geo::PolygonDataBuilderService < ServiceBase
       raise ServiceError, message
     end
 
+    ################################
+    # 2017.06.01 nyoshimatsu
+    ################################
+
+    # check KEY_CODE. count number of area with a KEY_CODE.
+    l   = []
+    h   = []
+    c   = []
+    i   = -1
+    idx = loc_idx
+    @rows.each do |l|
+      i   += 1
+      c[i] = 1
+  
+      if    l[idx].length == 9 then
+        c[i] = 0
+        @rows.each do |h|
+          if l[idx] == h[idx].slice(0,9) then
+            c[i] += 1
+          end
+        end
+      elsif l[idx].length == 5 then
+        c[i] = 0
+        @rows.each do |h|
+          if l[idx] == h[idx].slice(0,5) then
+            c[i] += 1
+          end
+        end
+      end
+    end
+
+    # generate a new area info into a array (s[]).
+    # there are single KEY_CODE area in the new area info.
+    # (no overlap area in with regard to KEY_CODE)
+
+    i = -1
+    j = -1
+    @rows.each do |l|
+      i += 1
+      if c[i] <= 1 then
+        j         += 1
+        @rows_s[j] = l
+      end
+    end
+
+    #####
+
     loc_hash = {} # KEY_CODEをキーとした値のリスト
-    @rows.each do |row|
+    @rows_s.each do |row|
       loc_hash[row[loc_idx]] = row
     end
 
     # 最大値最小値の計算
-    nums = @rows.collect{|row| row[count_idx].to_f}
+    nums = @rows_s.collect{|row| row[count_idx].to_f}
     min = nums.min
     max = nums.max
 
@@ -40,10 +88,21 @@ class Geo::PolygonDataBuilderService < ServiceBase
       next if loc_name.nil?
       
       loc_data = loc_hash[loc_name]
+
       next if loc_data.nil?
 
       header.each_with_index do |field, idx|
-        feature[:properties][field] = loc_data[idx].to_s
+
+        if ( ( field == 'CITYNAME' ) ||
+             ( field == 'NAME'     ) ||
+             ( field == '総人口'    ) ||
+             ( field == '男'       ) ||
+             ( field == '女'       ) ||
+             ( field == '世帯総数'  ) ) then
+
+          feature[:properties][field] = loc_data[idx].to_s
+          
+        end
       end
 
       # 地図塗り分け用の値作成
@@ -52,6 +111,11 @@ class Geo::PolygonDataBuilderService < ServiceBase
       feature[:properties][:'__range'] = range
 
       # feature[:properties].delete(:'__name')
+      feature[:properties].delete(:'KEN_NAME')
+      feature[:properties].delete(:'GST_NAME')
+      feature[:properties].delete(:'CSS_NAME')
+      feature[:properties].delete(:'MOJI')
+      feature[:properties].delete(:'HYOSYO')
     end
 
     @geojson
