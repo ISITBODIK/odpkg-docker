b6557573ce82d1b6cdd0d893d48377dbd1014e81
nyoshimatsu <nyoshimatsu@isit.or.jp>
2018-11-14 09:27:49 +0900

    ckanのharvest pluginのバージョンを指定。dashboardのcspをgoogleのポリシー変更に対応

--- dashboard/config/initializers/csp.rb
+++ dashboard/config/initializers/csp.rb
@@ -3,7 +3,7 @@ SecureHeaders::Configuration.default do |config|
     default_src: %w('self'),
     script_src: %w('self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://www.google-analytics.com),
     style_src: %w('self' 'unsafe-inline' https://fonts.googleapis.com),
-    img_src: %w('self' https://maps.googleapis.com https://maps.gstatic.com https://csi.gstatic.com https://www.google-analytics.com),
+    img_src: %w('self' data: https://*.googleapis.com https://maps.gstatic.com https://csi.gstatic.com https://www.google-analytics.com),
     font_src: %w('self' https://fonts.gstatic.com)
   }
 end
